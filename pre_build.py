cbfdc55db405d2080fc68fd3db7084941fb50129
Matthew Quinn <matthew.quinn@fanduel.com>
2024-07-10 16:48:35 +0100

    test

--- pre_build.py
+++ pre_build.py
@@ -6,7 +6,9 @@ from sys import exit
 
 def build() -> None:
     if which("yarn") is None:
-        print("Locust requires the yarn binary to be available in this shell to build the web front-end.\nSee: https://docs.locust.io/en/stable/developing-locust.html#making-changes-to-locust-s-web-ui")
+        print(
+            "Locust requires the yarn binary to be available in this shell to build the web front-end.\nSee: https://docs.locust.io/en/stable/developing-locust.html#making-changes-to-locust-s-web-ui"
+        )
         exit(1)
     if os.environ.get("SKIP_PRE_BUILD", "") == "true":
         print("Skipping front end build...")
